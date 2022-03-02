033f8f45cfdbafadb9c4610cb06fbcd0e6ad4011
adonis <goldendores9826@gmail.com>
2022-03-03 01:37:06 +0200

    discord

--- src/page/Discord.js
+++ src/page/Discord.js
@@ -4,7 +4,7 @@ import Joincommunityimg from "../assets/images/Joincommunity.png";
 const Discord = () => {
   return (
     <div>
-      <div className="w-full relative flex bg-[#182037]  pt-[105px] pb-4 justify-center" id="Discord">
+      <div className="w-full relative flex bg-[#182037]  pt-[105px] pb-[10px] justify-center" id="Discord">
         <div className="w-full">
           <div>
             <div className="flex justify-center md:hidden w-full  top-[65px] sm:top-[45px] md:top-[50px] lg:top-[86px] text-center font-bold font-sans text-white text-[30px]  mb-1">
@@ -33,10 +33,10 @@ const Discord = () => {
               alt=""
               className="min-w-full h-full px-[30px] md:px-[96px]"
             ></img>
-            <div className="hidden md:flex justify-center w-full absolute md:top-[25px] lg:top-[86px] text-center font-bold font-sans text-white text-[38px]  mb-1">
+            <div className="hidden md:flex justify-center w-full absolute md:top-[25px] lg:top-[115px] xl:top-[160px] 2xl:top-[185px] text-center font-bold font-sans text-white md:text-[38px]  lg:text-[35px] mb-1">
               Join The 
             </div>
-            <div className="hidden md:flex justify-center w-full absolute md:top-[65px] lg:top-[128px] text-center font-bold font-sans text-white text-[38px] mb-1">
+            <div className="hidden md:flex justify-center w-full absolute md:top-[65px] lg:top-[158px] xl:top-[205px] 2xl:top-[235px] text-center font-bold font-sans text-white md:text-[38px] lg:text-[35px] mb-1">
               HireCrypto Community
             </div>
             <div className="hidden md:flex flex   justify-center">
@@ -46,7 +46,7 @@ const Discord = () => {
                 rel="noreferrer"
                 className="flex justify-center"
               >
-                <button className="flex justify-center absolute  md:top-[128px] lg:top-[200px] shrink-0 px-4 py-1 font-semibold border border-transparent hover:border-gray-200 text-[30px] text-white active:text-red-600 bg-[#385d94] hover:bg-transparent rounded-md">
+                <button className="flex justify-center absolute  md:top-[128px] lg:top-[215px] xl:top-[265px] 2xl:top-[305px] shrink-0 px-4 py-1 font-semibold border border-transparent hover:border-gray-200 md:text-[30px] lg:text-[26px] text-white active:text-red-600 bg-[#385d94] hover:bg-transparent rounded-md">
                   Discord
                 </button>
               </a>
